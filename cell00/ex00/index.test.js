7b979ad6c6f5f0d975d2ad518e65b86c5f136c5f
Thitiwut Somsa <kizzaraiva@gmail.com>
2025-09-21 17:45:25 +0700

    init

---
+++ cell00/ex00/index.test.js
@@ -0,0 +1,16 @@
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require('path');
+const filePath = path.join(__dirname, './index.js');
+
+describe("cell00-ex00", () => {
+  test("file index.js exists", () => {
+    
+    const exists = fs.existsSync(filePath);
+    expect(exists).toBe(true);
+  });
+  test("prints Hello World", () => {
+    const output = execSync(`node ${filePath}`).toString().trim();
+    expect(output).toBe("Hello World");
+  });
+})
